b1d7102ddd29f77b78b2b9236dedd4b2c39cfbf9
BuildTools <ben.ja.wells@gmail.com>
2017-01-21 14:26:01 +0000

    ALL THE ART

---
+++ Assets/Script/DuckSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSprite : MonoBehaviour {
+
+	public SpriteRenderer body, outfit;
+
+	void Start () {
+
+		body.sprite = DuckSpriteManager.main.GetBody();
+		outfit.sprite = DuckSpriteManager.main.GetOutfit();
+
+	}
+
+}
---
+++ Assets/Script/DuckSpriteManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSpriteManager : MonoBehaviour {
+
+	public static DuckSpriteManager main; 
+	public Sprite[] body, outfits;
+
+	[Range(0f, 1f)]
+	public float specialChance = 0.3f;
+
+	void Start () {
+		if (main != null) {
+			Destroy(gameObject);
+		}
+		main = this;
+	}
+
+	void OnDestroy() {
+		if (main == this) {
+			main = null;
+		}
+	}
+
+	public Sprite GetBody() {
+		return RandomFromSpriteArray(body);
+	}
+
+	public Sprite GetOutfit() {
+		return RandomFromSpriteArray(outfits);
+	}
+
+	Sprite RandomFromSpriteArray(Sprite[] array) {
+		
+		if (array.Length > 1 && Random.Range(0f,1f) < specialChance) {
+
+			return array[Random.Range(1, array.Length)];
+
+		}
+
+		return array[0];
+	}
+
+}
