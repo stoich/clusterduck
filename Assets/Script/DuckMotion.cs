3edc145c93bc66a67ac31c8c98cfa6019e8f764c
BuildTools <ben.ja.wells@gmail.com>
2017-01-21 11:15:54 +0000

    Ducks bob

--- Assets/Script/DuckMotion.cs
+++ Assets/Script/DuckMotion.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class DuckMotion : MonoBehaviour {
 
 	public Rigidbody2D bodyWithSpeed;
-	public GameObject objToRotate;
-	float lowerLimit = 6f, upperLimit = 30f, maxRotation = 30f;
+	public GameObject objToRotate, objToBob;
+	float lowerLimit = 6f, upperLimit = 30f, maxRotation = 30f, bobFrequency = 1f, bobDisplacement = 0.05f;
+
+	float bobStage = 0f;
 
 	void Update () {
 
 		float rotation = bodyWithSpeed.velocity.magnitude;
 		
-print(rotation);
-
 		rotation -= lowerLimit;
 		rotation /= (upperLimit - lowerLimit);
 		rotation = Mathf.Clamp(rotation, 0f, 1f);
@@ -22,5 +22,14 @@ print(rotation);
 
 		objToRotate.transform.localEulerAngles = new Vector3(0f, 0f, rotation);
 	
+		bobStage = (bobStage + Time.deltaTime) % bobFrequency;
+		float bob = bobStage / bobFrequency;
+		bob *= 2f * Mathf.PI;
+		bob = Mathf.Sin(bob);
+		bob *= bobDisplacement;
+
+		objToRotate.transform.position = objToRotate.transform.parent.position + new Vector3(0f, bob, 0f);
+		print(objToRotate.transform.position);
+
 	}
 }
