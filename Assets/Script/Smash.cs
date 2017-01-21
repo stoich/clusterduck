b0baa6ec4be19cd71fac2ed95f1cb5c662d1efe2
Stoil <stoilstoychev@gmail.com>
2017-01-21 10:46:08 +0000

    Smashing the crates

---
+++ Assets/Script/Smash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smash : MonoBehaviour
+{
+    public float thresholdSpeed;
+    private Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //     print(body.velocity.magnitude);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Obstacle" && body.velocity.magnitude > 3)
+            Destroy(collision.gameObject);
+
+
+    }
+}
