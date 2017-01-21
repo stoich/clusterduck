24f317a405deab7f34a5eebbfbecb490e342c7ee
BuildTools <ben.ja.wells@gmail.com>
2017-01-21 18:20:43 +0000

    Game over dies

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	public static GameManager main; 
-	public Animator startGameAnimator;
+	public Animator startGameAnimator, gameOverAnimator;
 	public GameObject obstacleGenerator;
 	bool started;
 
-	bool gameStarted = false; 
+	bool gameStarted = false;
+	bool gameEnded = false;
+	bool readyToRestart = false;
 
 	void Awake () {
 		if (main != null) {
 			Destroy(gameObject);
 		}
 		main = this;
+	}
+
+	void Update() {
+		if (gameEnded && readyToRestart && Input.GetMouseButtonDown(0)) {
+			StartCoroutine(Restart());
+		}
 	}
 
 	void OnDestroy() {
@@ -26,13 +34,45 @@ public class GameManager : MonoBehaviour {
 
 	public void TryStartGame() {
 
-		print("Start?");
 		if (started) return;
 
-		print("Start!");
 		started = true;
 		obstacleGenerator.SetActive(true);
 		startGameAnimator.SetBool("started", true);
 
 	}
+
+	public void TryGameOver() {
+
+		if (!gameStarted || gameEnded) return;
+
+		gameEnded = true;
+		obstacleGenerator.SetActive(false);
+		gameOverAnimator.enabled = true;
+		gameOverAnimator.SetTrigger("isGameOver");
+
+		StartCoroutine(GameOverClickIgnore());
+
+	}
+
+	public void ConfirmGameOver() {
+
+
+
+	}
+
+	IEnumerator GameOverClickIgnore() {
+
+		yield return new WaitForSeconds(1f);
+		readyToRestart = true;
+	}
+
+	IEnumerator Restart() {
+
+		gameOverAnimator.SetBool("started", true);
+		yield return new WaitForSeconds(1f);
+		//restart the game
+
+	}
+
 }
