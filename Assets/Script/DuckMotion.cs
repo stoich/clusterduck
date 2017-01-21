64ebfad2181bf9f56f11ae60466d014802f98794
BuildTools <ben.ja.wells@gmail.com>
2017-01-21 11:41:41 +0000

    Fix duckmotion

--- Assets/Script/DuckMotion.cs
+++ Assets/Script/DuckMotion.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DuckMotion : MonoBehaviour
-{
+public class DuckMotion : MonoBehaviour {
 
-    public Rigidbody2D bodyWithSpeed;
-    public GameObject objToRotate;
-    float lowerLimit = 6f, upperLimit = 30f, maxRotation = 30f;
+	public Rigidbody2D bodyWithSpeed;
+	public GameObject objToRotate, objToBob;
+	float lowerLimit = 6f, upperLimit = 30f, maxRotation = 30f, bobFrequency = 1f, bobDisplacement = 0.05f;
 
-    void Update()
-    {
-        float rotation = bodyWithSpeed.velocity.magnitude;
+	float bobStage = 0f;
 
-        //print(rotation);
+	void Update () {
 
-        rotation -= lowerLimit;
-        rotation /= (upperLimit - lowerLimit);
-        rotation = Mathf.Clamp(rotation, 0f, 1f);
-        rotation = Mathf.Pow(rotation, 0.5f);
-        rotation *= maxRotation;
+		float rotation = bodyWithSpeed.velocity.magnitude;
+		
+		rotation -= lowerLimit;
+		rotation /= (upperLimit - lowerLimit);
+		rotation = Mathf.Clamp(rotation, 0f, 1f);
+		rotation = Mathf.Pow(rotation, 0.5f);
+		rotation *= maxRotation;
 
-        objToRotate.transform.localEulerAngles = new Vector3(0f, 0f, rotation);
-    }
+		objToRotate.transform.localEulerAngles = new Vector3(0f, 0f, rotation);
+	
+		bobStage = (bobStage + Time.deltaTime) % bobFrequency;
+		float bob = bobStage / bobFrequency;
+		bob *= 2f * Mathf.PI;
+		bob = Mathf.Sin(bob);
+		bob *= bobDisplacement;
+
+		objToRotate.transform.position = objToRotate.transform.parent.position + new Vector3(0f, bob, 0f);
+		print(objToRotate.transform.position);
+
+	}
 }
