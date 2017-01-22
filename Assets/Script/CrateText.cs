fedfeae3700b2fc8cdf82539cd8972e0cd992e14
BuildTools <ben.ja.wells@gmail.com>
2017-01-22 11:17:06 +0000

    I renamed the graphics for fun

---
+++ Assets/Script/CrateText.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+ 
+ public class CrateText : MonoBehaviour {
+     public float delay = 0f;
+     public Text textBox;
+ 
+     // Use this for initialization
+     void Start () {
+         Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay); 
+     }
+
+     public void Setup(Vector3 position, string text) {
+     	transform.position = position;
+     	textBox.text = text;
+     }
+ }
